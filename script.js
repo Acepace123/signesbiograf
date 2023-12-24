adfed00f81369e6b0655216dedab2a740e4d98c7
Acepace123 <149021059+Acepace123@users.noreply.github.com>
2023-12-24 20:54:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function clearPage() {
 function showVideo() {
     document.querySelector("button").remove();
     const video = document.createElement("iframe");
-    video.src = "https://www.youtube.com/embed/ivtIWb3m7uA";
+    video.src = "https://www.youtube.com/embed/gV84LRa7GSo";
     video.height = "0";
     video.width = "0";
     video.allow = "fullscreen";
