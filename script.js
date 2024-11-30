859427ddc5dfaa62c4f43debb5f714ec4e3a9882
Acepace123 <149021059+Acepace123@users.noreply.github.com>
2024-11-30 21:32:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function clearPage() {
 function showVideo() {
     document.querySelector("button").remove();
     const video = document.createElement("iframe");
-    video.src = "https://www.youtube.com/embed/lD2ucI15pdk";
+    video.src = "https://vimeo.com/1034820742/8eb1eae488?ts=0&share=copy";
     video.height = "0";
     video.width = "0";
     video.allow = "fullscreen";
